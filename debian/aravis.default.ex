f038ca3db165e3e088f5e3716991fcff03401409
Troy D. Straszheim <troy@resophonic.com>
2016-10-08 15:03:37 -0700

    starting debian goodness

---
+++ debian/aravis.default.ex
@@ -0,0 +1,10 @@
+# Defaults for aravis initscript
+# sourced by /etc/init.d/aravis
+# installed at /etc/default/aravis by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+libaravis 0.5.4 aravis (>> 0.5.4-0), aravis (<< 0.5.4-99)
